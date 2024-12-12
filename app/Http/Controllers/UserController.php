db808208265568a0d5a4428e97dc61e4918455a4
julioSilvaDias <juliocesar.silvadi@elorrieta-errekamari.com>
2024-12-12 19:57:20 +0100

    Crud
    Crud del modelo User terminado, falta configurar las rutas

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::orderBy('id')->get();
+        return view('users.index', ['users' => $users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->user = $request->user;
+        $user->tel1 = $request->tel1;
+        $user->tel2 = $request->tel2;
+        $user->address = $request->address;
+        $user->dni = $request->dni;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return view('users.show',['user'=>$user]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', ['user'=>$user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->user = $request->user;
+        $user->tel1 = $request->tel1;
+        $user->tel2 = $request->tel2;
+        $user->address = $request->address;
+        $user->dni = $request->dni;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        return view('users.show',['user'=>$user]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+}
