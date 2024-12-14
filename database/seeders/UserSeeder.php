8ea03d8882940a919deb45994e3293ecd4f4d03f
Snaider <snaiderviracocha@gmail.com>
2024-12-14 20:46:50 +0100

    Cambios minimos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
@@ -24,14 +25,32 @@ class UserSeeder extends Seeder
             "name"=>"godName",
             "email"=>"GodEmail@gmail.com",
             "password"=>"admin",
+            "remember_token"=>Str::random(10),
+            "created_at"=>now(),
+            'role_id'=> 1,
         ]);
 
-        User::factory()->count(50)->state([
-            'role_id' => 4,
+        DB::table('users')->insert([
+            "user"=>"Snaider",
+            "surname"=>"Luna",
+            "tel-1"=>"999",
+            "tel-2"=>"777",
+            "address"=>"Elorrieta",
+            "dni"=>"000000S",
+            "name"=>"Jordy",
+            "email"=>"Luna@gmail.com",
+            "password"=>"Snaider",
+            "remember_token"=>Str::random(10),
+            "created_at"=>now(),
+            'role_id' => 2,
+        ]);
+
+        User::factory()->count(3)->state([
+            'role_id' => 3,
         ])->create();
 
-        User::factory()->count(20)->state([
-            'role_id' => 3,
+        User::factory()->count(10)->state([
+            'role_id' => 4,
         ])->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RolController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::controller(RolController::class)->group(function () {
-    Route::get('/roles', 'index')->name('roles.index');
 });
