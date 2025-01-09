2da9af3bb98f7ca42c03e8f402c6551b4a82a44a
Snaider <snaiderviracocha@gmail.com>
2025-01-09 19:46:07 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\API\Asignatura_Usuario_HorarioController;
 use App\Http\Controllers\API\AsignaturaController;
 use App\Http\Controllers\API\CicloController;
 use App\Http\Controllers\API\CicloUsuarioController;
+use App\Http\Controllers\API\HorarioController;
+use App\Http\Controllers\API\RolController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ReunionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +19,9 @@ Route::apiResource('asignatura-usuario-horario', Asignatura_Usuario_HorarioContr
 Route::apiResource('asignatura', AsignaturaController::class);
 Route::apiResource('ciclo', CicloController::class);
 Route::apiResource('ciclo-usuario', CicloUsuarioController::class);
+
+
+Route::apiResource('rol', RolController::class);
+Route::apiResource('reunion', ReunionController::class);
+Route::apiResource('user', UserController::class);
+Route::apiResource('horario', HorarioController::class);
