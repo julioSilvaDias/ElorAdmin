0300ebe150b15497de8a6243320a6499a028f206
Maiory1 <156131673+Maiory1@users.noreply.github.com>
2025-01-09 16:13:47 +0100

    API Creación de la API

---
+++ app/Http/Controllers/API/Asignatura_Usuario_HorarioController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Asignatura_Usuario_Horario;
+use Illuminate\Http\Request;
+
+class Asignatura_Usuario_HorarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Asignatura_Usuario_Horario $asignatura_Usuario_Horario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Asignatura_Usuario_Horario $asignatura_Usuario_Horario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Asignatura_Usuario_Horario $asignatura_Usuario_Horario)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/HorarioController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Horario;
+use Illuminate\Http\Request;
+
+class HorarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Horario $horario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Horario $horario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Horario $horario)
+    {
+        //
+    }
+}
