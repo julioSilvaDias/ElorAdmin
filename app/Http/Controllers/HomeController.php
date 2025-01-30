4f1a916de0d3abe7b47218842ecca9c609802873
Snaider <snaiderviracocha@gmail.com>
2025-01-30 19:53:44 +0100

    Se ha borrado script de js innecesario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware(function ($request, $next) {
             $homeUrl = route('home');
 
--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -23,7 +23,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
 
